3978a1216bdb554e778bffdcba25eaf27d1b1f43
sarthakReldyn <sarthak@reldyn.co>
2024-07-30 17:10:58 +0530

    Added Student table and inserted first record

--- src/main/java/com/web/reactive/programming/entities/Student.java
+++ src/main/java/com/web/reactive/programming/entities/Student.java
@@ -1,6 +1,7 @@
 package com.web.reactive.programming.entities;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 import lombok.AllArgsConstructor;
@@ -8,11 +9,16 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @AllArgsConstructor
-@Table
+@Table("students")
 public class Student {
 
     @Id
+    @Column("id")
     private String id;
 
+    @Column("student_name")
     private String name;
+
+    @Column("total_marks")
+    private int marks;
 }
--- src/main/resources/db/migration/V1__create_table_students.sql
+++ src/main/resources/db/migration/V1__create_table_students.sql
@@ -1,4 +1,8 @@
 CREATE TABLE students (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
+    id VARCHAR(80) PRIMARY KEY,
+    student_name VARCHAR(255) NOT NULL,
+    total_marks INT NOT NULL
 );
+
+INSERT INTO students (id, student_name, total_marks)
+VALUES ('550e8400-e29b-41d4-a716-446655440000', 'John Doe', 85);
